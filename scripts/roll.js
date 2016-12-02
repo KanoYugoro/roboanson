4b3186b47e69160b32c0ef52ad901bc47f69d089
Anson Wayman <Anson.Wayman@extendhealth.com>
2016-12-02 15:31:23 -0700

    fixed roll

--- scripts/roll.js
+++ scripts/roll.js
@@ -1,15 +1,11 @@
 "use strict";
 
-// Inspired by the hubot pugme package, but there is no limit on its pugbomb functionality
-// and I considered that dangerous.  So I made a quick adjustment and rewrote it in javascript.
-var request = require('request');
-
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
 exports["default"] = function (robot) {
-  robot.hear(/roll( (\d+))?.+/i, function(res) {
+  robot.hear(/roll( (\d+))?/i, function(res) {
     var numDigits = Number(res.match[2]) || 3;
     if (numDigits > 9) {
       numDigits = 9;
